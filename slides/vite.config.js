4773c83440c6f5a6151c57729c0aff75a5922764
Jan Gregor Emge-Triebel <jan@jangregor.me>
2026-02-16 14:32:31 +0100

    Improved asset handling

---
+++ slides/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                { src: './../public/*', dest: '.' }
+            ]
+        })
+    ]
+});
---
+++ theme/components/CustomImage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">/**
+ * We need this component to handle different url paths for each slide set
+ */
+import {computed} from "vue";
+
+let props = defineProps<{ src: string }>()
+
+const resolvedUrl = computed(() => {
+  if (props.src.startsWith('/'))
+    return import.meta.env.BASE_URL + props.src.slice(1)
+
+  return props.src
+})
+</script>
+
+<template>
+  <img :src="resolvedUrl" />
+</template>
+
+<style scoped>
+</style>
