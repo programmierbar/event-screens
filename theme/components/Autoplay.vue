dff4a5550bf492874fa0077e7ddf82ed47646fc6
Jan Gregor Emge-Triebel <jan@jangregor.me>
2026-02-27 09:01:34 +0100

    Added hard reload logic to force eventual refresh

--- theme/components/Autoplay.vue
+++ theme/components/Autoplay.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {onSlideEnter, onSlideLeave, useNav} from '@slidev/client'
+import {ref} from "vue";
 
 const nav = useNav()
 
@@ -16,23 +17,39 @@ const props = defineProps({
     type: Number,
     default: 1,
   },
+  refreshAfter: {
+    type: Number,
+    default: 10,
+  },
 })
 
-let autplayTimer = null;
+let autoplayTimer = null;
+
+const runs = ref(0);
 
 onSlideEnter(() => {
   if (props.enabled !== true) return;
-  autplayTimer = setTimeout(() => {
+  autoplayTimer = setTimeout(() => {
     if (nav.hasNext.value) {
       nav.nextSlide()
     } else {
+      runs.value++;
+      // if enough runs have passed, force refresh of page to reload content
+      if (runs.value >= props.refreshAfter) {
+        runs.value = 0;
+        const url = new URL(window.location.toString());
+        const segments = url.pathname.split('/');
+        segments.pop();
+        url.pathname = segments.join('/');
+        window.location.assign(url.toString());
+      }
       nav.go(props.restartAt)
     }
   }, props.seconds * 1000)
 })
 
 onSlideLeave(() => {
-  clearTimeout(autplayTimer)
+  clearTimeout(autoplayTimer)
 })
 </script>
 
